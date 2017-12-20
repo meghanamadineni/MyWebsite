1d611e8856f2ad273f87fbd299c029ba3cca5350
Meghana Madineni <meghanamadineni@gmail.com>
2017-12-20 17:28:31 +0530

    heroku ready

--- src/app/service/contact.service.ts
+++ src/app/service/contact.service.ts
@@ -10,7 +10,7 @@ export class ContactService {
 
     constructor(private http: Http) { }
 
-    private serverApi= 'https://localhost:8080';
+    private serverApi= 'https://meghanamadineni.herokuapp.com:8080';
 
     
  // post("/api/contacts")
