785943149633137f047e650681ba83fe0c4fb53c
Meghana Madineni <meghanamadineni@gmail.com>
2017-12-20 18:11:36 +0530

    heroku ready

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongodb.MongoClient.connect("mongodb://admin:admin@ds113825.mlab.com:13825/websi
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(8080, function () {
+  var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
--- src/app/service/contact.service.ts
+++ src/app/service/contact.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Http,Headers } from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import { Contact } from '../model/contact'
-import { Http, Response, Headers } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -10,19 +10,10 @@ export class ContactService {
 
     constructor(private http: Http) { }
 
-    private serverApi= 'https://localhost:8080';
+    private serverApi= 'http://localhost:8080';
 
     
- // post("/api/contacts")
-//  createContact(newContact: Contact): Promise<Contact> {
-// 	let URI = `${this.serverApi}/contact/`;
-// 	let headers = new Headers;
-// 	headers.append('Content-Type', 'application/json');
-// 	return this.http.post(URI, newContact,{headers:headers})
-// 			   .toPromise()
-// 			   .then(response => response.json() as Contact)
-// 			   .catch();
-//   }
+
 
     	public addContact(contact: Contact) {
   		let URI = `${this.serverApi}/contact/`;
